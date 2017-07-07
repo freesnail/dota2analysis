a10a3ce0066239ad3b377882e13dee9de0c5dc29
Jie Zhou <zhojie@codeaurora.org>
2017-07-07 18:51:20 +0800

    #save

--- app/analysis.py
+++ app/analysis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from __future__ import division
 import os, sys, signal
 import time, threading
@@ -7,6 +8,7 @@ from multiprocessing.dummy import Pool as ThreadPool
 import dota2api
 import logging
 import logging.config
+import pymysql
 
 #game level definition
 ANY = 0
@@ -26,11 +28,13 @@ RECENT_VALID_MATCH_ID = 3017815676
 CAMP_RADIANT = 0
 CAMP_DIRE = 1
 HERO_WINNING_RATE = {heroid: [0, 0] for heroid in range(HERO_ID_BASE, HERO_ID_LAST)}
-ACCOUNT = []
+LEVEL = [NORMAL, HIGH, VERY_HIGH]
 #THREAD_COUNT = 14
 
 api = dota2api.Initialise("0FC7F27CF84F15C2492359A7D52C10BC")
 lock = threading.Lock()
+DB = pymysql.connect("localhost","root","123456", "dota2")
+cursor = DB.cursor()
 
 #watcher to response keybord interrupt
 class Watcher():  
@@ -63,7 +67,7 @@ def get_player_camp (slot):
     else:
         return CAMP_DIRE
 
-def process_hero_result(hero_id, hero_camp, radiant_win):
+def process_hero_result(hero_id, hero_camp, radiant_win, level):
     global HERO_WINNING_RATE
     if radiant_win:
         if hero_camp == CAMP_RADIANT:
@@ -82,73 +86,51 @@ def is_AI_match(match):
     else:
         return False
 
-def process_matches (result):
-    print "match count: %d" % result["num_results"]
-    for i in range (0, int(result["num_results"]) - 1):
-        matchid = result["matches"][i]["match_id"] 
-        match = api.get_match_details(match_id = matchid)
-        if is_AI_match (match):
-            continue
-        if match:
-            for i in range(0, 10):
-                hero_id = match["players"][i]["hero_id"]
-                camp = get_player_camp(match["players"][i]["player_slot"])
-                lock.acquire()
-                try:
-                    process_hero_result (hero_id, camp, match["radiant_win"])
-                finally:
-                    lock.release()
-    return 0
-
-def open_account_file():
-    global ACCOUNT_FILE
+def match_processed(match_id):
+    sql = "INSERT IGNORE INTO matches (match_id) VALUES (%d)" % (match_id)
     try:
-        ACCOUNT_FILE = open('/home/jie/py/dota2analysis/data/account.txt', 'r')
+        cursor.execute(sql)
+        DB.commit()
     except:
-        try:
-            ACCOUNT_FILE = open('/Users/Raspberrypi/py/dota2analysis/data/account.txt', 'r')
-        except:
-            logging.error("cannot find account file")    
-            return -1
+        DB.rollback()
+
+    return False
+
+def process_matches (result, level):
+    print (result["num_results"], level)
+    for i in range (0, int(result["num_results"])):
+        match_id = (int)(result["matches"][i]["match_id"])
+        print (match_id)
+        if not match_processed(match_id):
+            match = api.get_match_details(match_id = match_id)
+            if is_AI_match (match):
+                continue
+            if match:
+                for i in range(0, 10):
+                    hero_id = match["players"][i]["hero_id"]
+                    camp = get_player_camp(match["players"][i]["player_slot"])
+                    lock.acquire()
+                    try:
+                        process_hero_result (hero_id, camp, match["radiant_win"], level)
+                    finally:
+                        lock.release()
     return 0
 
-def close_account_file(account_file):
-    ACCOUNT_FILE.close()
-
-def process_account_match(account):
-    logging.debug ("processing account: %d" % account)
-    result = api.get_match_history(account_id = account, skill = VERY_HIGH, start_at_match_id = RECENT_VALID_MATCH_ID, matches_requested = 50)
-    #result = api.get_match_history(game_mode = 2, skill = VERY_HIGH, matches_requested = 1000)
-    rate = process_matches(result)
-    logging.debug ("finished processing account: %d" % account)
+def process_level_match (level):
+    result = api.get_match_history (skill = level, matches_requested = 50)
+    process_matches (result, level);
+    logging.debug ("finished processing level: %d" % level)
 
 def main():
-    global ACCOUNT
-    account_num = 0
 
-    Watcher()
-
-    if open_account_file() < 0:
-        return -1
-
-#read account id 
-    while True:
-        line = ACCOUNT_FILE.readline()
-        if not line:
-            logging.info("%d accounts loaded" % account_num)
-            break
-        else:
-            account_num += 1
-            account = int(line)
-            ACCOUNT.append(account)
     pool = ThreadPool()
-    results = pool.map(process_account_match, ACCOUNT)
+
+    results = pool.map(process_level_match, LEVEL)
+
     pool.close() 
     pool.join()
-
-    print HERO_WINNING_RATE
-    close_account_file(ACCOUNT_FILE)
-
+    DB.close()
+    #print (HERO_WINNING_RATE)
     return 0
 
 if __name__ == "__main__":
